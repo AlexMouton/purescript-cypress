6c16df0aef62d24313853f5c5edccdc01cd29385
Alex Mouton <alex.mouton@originate.com>
2020-08-09 13:11:39 -0700

    Move amazon  Test out of src.

--- test/Main.purs
+++ test/Main.purs
@@ -3,9 +3,25 @@ module Test.Main where
 import Prelude
 
 import Effect (Effect)
-import Effect.Class.Console (log)
+import Cypress (Cy, CypressM, click, exec, first, get, go, runCypress, should, thn, typ, visit, (~))
+import Cypress as C
 
-main :: Effect Unit
-main = do
-  log "🍝"
-  log "You should add some tests."
+amazon :: CypressM Unit
+amazon = do
+  visit "/"
+  _ <- get "a.a-carousel-goto-prevpage"
+    ~ should "have.length" 1
+    ~ first
+    ~ click
+  _ <- get "#twotabsearchtextbox"
+    ~ typ "HEHEHE"
+  visit "/dp/B07X6C9RMF/ref=ods_gw_vicc_blinkmini_apr_anc"
+  go "back"
+
+  exec "ls -la"
+    ~ thn _.stdout
+    ~ C.log
+
+main :: Cy -> Effect Unit
+main cy = do
+  runCypress amazon cy
