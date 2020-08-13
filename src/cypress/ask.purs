bb726d0a4244fc34ed61bbc791f3c523d4b03d21
Alex Mouton <alex.mouton@originate.com>
2020-08-12 18:02:39 -0700

    Add two  more asks

--- src/cypress/ask.purs
+++ src/cypress/ask.purs
@@ -3,7 +3,7 @@ module Cypress.Ask where
 import Prelude
 -- import Data.Functor (class Functor)
 import Effect (Effect)
-import Effect.Uncurried (EffectFn1, runEffectFn1, EffectFn2, runEffectFn2, EffectFn3, runEffectFn3)
+import Effect.Uncurried (EffectFn1, runEffectFn1, EffectFn2, runEffectFn2, EffectFn3, runEffectFn3, EffectFn4, runEffectFn4, EffectFn5, runEffectFn5)
 import Control.Monad.Reader.Trans (ReaderT(..), ask)
 
 askC1 :: forall b a. (EffectFn1 b a) -> ReaderT b Effect a
@@ -20,6 +20,17 @@ askC3 :: forall s t a b. (EffectFn3 s t b a) -> s -> t -> ReaderT b Effect a
 askC3 f s t = ReaderT $ do
   cy <- ask
   pure $ (runEffectFn3 f) s t cy
+
+askC4 :: forall s t u a b. (EffectFn4 s t u b a) -> s -> t -> u -> ReaderT b Effect a
+askC4 f s t u = ReaderT $ do
+  cy <- ask
+  pure $ (runEffectFn4 f) s t u cy
+
+askC5 :: forall s t u v a b. (EffectFn5 s t u v b a) -> s -> t -> u -> v -> ReaderT b Effect a
+askC5 f s t u v = ReaderT $ do
+  cy <- ask
+  pure $ (runEffectFn5 f) s t u v cy
+
 
 naskC1 :: forall s a b. (EffectFn1 s a) -> s -> ReaderT b Effect a
 naskC1 f s = ReaderT $ do
